fd993cb8ebf845d79c932be84366b62fdc17bddc
Daniel Marinho <danielmcneto@gmail.com>
2026-08-05 10:47:26 -0300

    Finish the title screen
    Built custom buttons with texture(and hover texture)
    
    quit button works
    start button works

--- Global.gd
+++ Global.gd
@@ -1,5 +1,7 @@
-extends Node2D
+extends Node
 
+var lives = 5
+var minigames_done = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,11 +11,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_sart_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
